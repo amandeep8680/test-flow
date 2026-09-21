6563125f8e6ab45487cbe50d57aed61e3fb4c04e
Amandeep SIngh <adeepsingh2053@gmail.com>
2026-09-21 12:26:26 +0530

    CORS Added

--- app/main.py
+++ app/main.py
@@ -3,12 +3,12 @@ from sqlalchemy import text
 
 from app.api.v1.router import api_router
 from app.core.database import AsyncSessionLocal
-
+from core.cors import setup_cors
 
 app = FastAPI(
     title="Test Flow API",
     version="1.0.0",
 )
-
+setup_cors(app)
 
 app.include_router(api_router)
